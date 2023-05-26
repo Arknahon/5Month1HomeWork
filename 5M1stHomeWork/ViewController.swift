5f74cd8bc43099f50818a58160d717d30e97228d
Arknahon <bakirov090303@gmail.com>
2023-05-26 15:47:40 +0600

    Initial Commit

---
+++ 5M1stHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5M1stHomeWork
+//
+//  Created by user on 26/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
